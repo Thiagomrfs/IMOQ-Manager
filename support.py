a35925c68e835289b0403705358c95060cea5421
Thiagomrfs <thiagomarbsn@gmail.com>
2020-05-28 19:37:00 -0300

    improved msg func

--- support.py
+++ support.py
@@ -26,5 +26,6 @@ def menu(title, *options):
     for option in options:
         print(f"\033[34m{count}\033[m - \033[33m{option}\033[m")
         count +=1
+    print()
     resp = verify_int(input("Please choose an option: "), range(1, count+1))
     return resp
