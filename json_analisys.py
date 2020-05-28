64a02611cc356ba62150e1afd5f91a78f7a33878
Thiagomrfs <thiagomarbsn@gmail.com>
2020-05-28 19:53:20 -0300

    Creating database if it does not exists

--- json_analisys.py
+++ json_analisys.py
@@ -6,12 +6,20 @@ def read():
         data = json.load(db)
     print(data)
 
-def write(key, value):
+def write_json(new_data):
     with open("database.json", "r+") as db:
         data = json.load(db)
 
-    data.append({key: value})
+    data.append(new_data)
 
     with open("database.json", "w+") as db:
         json.dump(data, db, indent=4, separators=(",", ":"))
 
+try: 
+    db = open("database.json")
+    json.load(db)
+    db.close()
+except:
+    db = open("database.json", "a+")
+    db.write("[]")
+    db.close()
